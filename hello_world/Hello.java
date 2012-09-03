110178a8f8aac1afdce5fd5d8fba5269c4de4b49
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-09-03 20:26:04 +0200

    Hello World in Hello-World

---
+++ hello_world/Hello.java
@@ -0,0 +1,12 @@
+/* 
+ * Hello-World-Programm
+ * Vorkurs Informatik
+ * Autor: Sebastian Mai
+ *
+ */
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello World!"); // gibt "Hello World" aus.
+	}
+}
