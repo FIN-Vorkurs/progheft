34c5a77a1ca508e1bd7c046603b19c0a164dee90
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-09-05 15:27:41 +0200

    Shopping Carts aufgabe

---
+++ funktionen/ShoppingCart.java
@@ -0,0 +1,20 @@
+public class ShoppingCart {
+/* 
+ * ShoppingCart.java
+ * Moves ShoppingCarts from stack to stack
+ *
+ */
+
+	public static void main(String args[]) {
+		int[] rows = {3, 14, 15, 9, 2}
+
+		
+		// this draws all the ShoppingCarts to the console
+		for(int i = 0; i<rows.length; i++) {
+			for(int j = 0; j < reihe[i]; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
+}
