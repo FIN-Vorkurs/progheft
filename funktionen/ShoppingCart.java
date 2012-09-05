7e8772ab89c3bedf8e474faa0354e7b302b30b67
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-09-05 15:59:33 +0200

    verbesserungen im shoppingcart iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- funktionen/ShoppingCart.java
+++ funktionen/ShoppingCart.java
@@ -6,15 +6,17 @@ public class ShoppingCart {
  */
 
 	public static void main(String args[]) {
-		int[] rows = {3, 14, 15, 9, 2}
+		int[] rows = {3, 14, 15, 9, 2};
 
 		
 		// this draws all the ShoppingCarts to the console
+		System.out.println("----------------------");
 		for(int i = 0; i<rows.length; i++) {
-			for(int j = 0; j < reihe[i]; j++) {
+			for(int j = 0; j < rows[i]; j++) {
 				System.out.print("#");
 			}
 			System.out.println();
 		}
+		System.out.println("----------------------");
 	}
 }
